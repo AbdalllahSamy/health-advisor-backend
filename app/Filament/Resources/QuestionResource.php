e539ec6f68555717a483be31b6e5dd73c1462721
abdalllahsamy <abdosamu538@gmail.com>
2025-06-09 21:27:31 +0300

    quesitons

--- app/Filament/Resources/QuestionResource.php
+++ app/Filament/Resources/QuestionResource.php
@@ -13,6 +13,7 @@ use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -60,7 +61,14 @@ class QuestionResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('id')
+                    ->label('#ID')
+                    ->limit(50)
+                    ->tooltip(fn($record) => $record->id),
+                TextColumn::make('question')
+                    ->label('Question')
+                    ->limit(50)
+                    ->tooltip(fn($record) => $record->question),
             ])
             ->filters([
                 //
